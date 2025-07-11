92f8bc3c43db4de66783454b9a11291454d44456
Kriti Sunkara <karasunshine0@gmail.com>
2025-07-11 10:24:47 -0400

    Initial Commit

---
+++ Birthdays3/Birthdays3App.swift
@@ -0,0 +1,17 @@
+//
+//  Birthdays3App.swift
+//  Birthdays3
+//
+//  Created by Scholar on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Birthdays3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
